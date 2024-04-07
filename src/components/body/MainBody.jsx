839a2dcaf620f341bbb9531628d6caea70c2ab29
Ajay P Gujjar <ajaypgujjar@yahoo.com>
2024-04-07 22:59:27 +0530

    added 9 more cards

--- src/components/body/MainBody.jsx
+++ src/components/body/MainBody.jsx
@@ -4,14 +4,40 @@ import '../Styles.css'
 export default function MainBody() {
     return (
         <div className='container'>
-            <div className='row'>
+            <div className='row application-cards'>
                 <div className='col-sm'>
-                    <div className="shadow-lg p-3 mb-5 bg-white rounded" style={{ margin: '50px' }}>Application 01</div>
+                    <div className="shadow p-3 mb-5 bg-white rounded cardsize">Application 01</div>
                 </div>
                 <div className='col-sm'>
-                    <div className="shadow p-3 mb-5 bg-white rounded" style={{ margin: '50px' }}>Application 02</div>
+                    <div className="shadow p-3 mb-5 bg-white rounded cardsize">Application 02</div>
                 </div>
-                <div className='col-sm'></div>
+                <div className='col-sm'>
+                    <div className="shadow p-3 mb-5 bg-white rounded cardsize">Application 03</div>
+                </div>
+            </div>
+
+            <div className='row application-cards'>
+                <div className='col-sm'>
+                    <div className="shadow p-3 mb-5 bg-white rounded cardsize">Application 04</div>
+                </div>
+                <div className='col-sm'>
+                    <div className="shadow p-3 mb-5 bg-white rounded cardsize">Application 05</div>
+                </div>
+                <div className='col-sm'>
+                    <div className="shadow p-3 mb-5 bg-white rounded cardsize">Application 06</div>
+                </div>
+            </div>
+
+            <div className='row application-cards'>
+                <div className='col-sm'>
+                    <div className="shadow p-3 mb-5 bg-white rounded cardsize">Application 07</div>
+                </div>
+                <div className='col-sm'>
+                    <div className="shadow p-3 mb-5 bg-white rounded cardsize">Application 08</div>
+                </div>
+                <div className='col-sm'>
+                    <div className="shadow p-3 mb-5 bg-white rounded cardsize">Application 09</div>
+                </div>
             </div>
         </div>
     )
