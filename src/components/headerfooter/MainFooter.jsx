4418c3f18aecb9e96135cddb1333202b2ce16fd3
Ajay P Gujjar <ajaypgujjar@yahoo.com>
2024-04-08 01:43:03 +0530

    added footer to the page

--- src/components/headerfooter/MainFooter.jsx
+++ src/components/headerfooter/MainFooter.jsx
@@ -3,6 +3,9 @@ import React from 'react';
 export default function MainFooter() {
   return (
     <div>
+      <footer class="bg-light text-black text-center py-3">
+        <p>&copy; 2024 Company Name Not Yet Decided</p>
+      </footer>
     </div>
   )
 }
