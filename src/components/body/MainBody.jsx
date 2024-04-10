39aec6442d23bbafc32c46454419646a87d2a9ad
Ajay P Gujjar <ajaypgujjar@yahoo.com>
2024-04-10 21:57:47 +0530

    added onhover card func

--- src/components/body/MainBody.jsx
+++ src/components/body/MainBody.jsx
@@ -5,38 +5,38 @@ export default function MainBody() {
     return (
         <div className='container'>
             <div className='row application-cards'>
-                <div className='col-sm'>
-                    <div className="shadow p-3 mb-5 bg-white rounded cardsize">Application 01</div>
+                <div className='col-sm card-row-margin'>
+                    <div className="cardsize">Application 01</div>
                 </div>
-                <div className='col-sm'>
-                    <div className="shadow p-3 mb-5 bg-white rounded cardsize">Application 02</div>
+                <div className='col-sm card-row-margin'>
+                    <div className="cardsize">Application 02</div>
                 </div>
-                <div className='col-sm'>
-                    <div className="shadow p-3 mb-5 bg-white rounded cardsize">Application 03</div>
-                </div>
-            </div>
-
-            <div className='row application-cards'>
-                <div className='col-sm'>
-                    <div className="shadow p-3 mb-5 bg-white rounded cardsize">Application 04</div>
-                </div>
-                <div className='col-sm'>
-                    <div className="shadow p-3 mb-5 bg-white rounded cardsize">Application 05</div>
-                </div>
-                <div className='col-sm'>
-                    <div className="shadow p-3 mb-5 bg-white rounded cardsize">Application 06</div>
+                <div className='col-sm card-row-margin'>
+                    <div className="cardsize">Application 03</div>
                 </div>
             </div>
 
             <div className='row application-cards'>
-                <div className='col-sm'>
-                    <div className="shadow p-3 mb-5 bg-white rounded cardsize">Application 07</div>
+                <div className='col-sm card-row-margin'>
+                    <div className="cardsize">Application 04</div>
                 </div>
-                <div className='col-sm'>
-                    <div className="shadow p-3 mb-5 bg-white rounded cardsize">Application 08</div>
+                <div className='col-sm card-row-margin'>
+                    <div className="cardsize">Application 05</div>
                 </div>
-                <div className='col-sm'>
-                    <div className="shadow p-3 mb-5 bg-white rounded cardsize">Application 09</div>
+                <div className='col-sm card-row-margin'>
+                    <div className="cardsize">Application 06</div>
+                </div>
+            </div>
+
+            <div className='row application-cards'>
+                <div className='col-sm card-row-margin'>
+                    <div className="cardsize">Application 07</div>
+                </div>
+                <div className='col-sm card-row-margin'>
+                    <div className="cardsize">Application 08</div>
+                </div>
+                <div className='col-sm card-row-margin'>
+                    <div className="cardsize">Application 09</div>
                 </div>
             </div>
         </div>
